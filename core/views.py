da821f2bf18c7a3ee20b0481c07510c9668878c4
TEP Punloeu <billboxfather@gmail.com>
2023-01-16 23:53:13 +0700

    Update bug in core direcory

--- core/views.py
+++ core/views.py
@@ -1,9 +1,3 @@
-from django.conf.urls import url
-from rest_framework_swagger.views import get_swagger_view
+from django.shortcuts import render
 
-schema_view = get_swagger_view(title='Pastebin API')
-
-urlpatterns = [
-    url(r'^$', schema_view)
-]
-###
+# Create your views here.
